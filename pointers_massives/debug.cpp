247d87856eba8c4dd45e1bb152f9e415d03ab3d8
Jentlebright <danila.shpak@list.ru>
2026-01-04 21:38:05 +0300

    init

--- pointers_massives/debug.cpp
+++ pointers_massives/debug.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main() {
-    int* apartment = new int; // Арендовали квартиру под int  
-    *apartment = 42; // Завезли диван, телевизор и 42  
-    cout << "Указывает на адрес: " << apartment << endl; // Смотрим, где нашлось местечко
-    cout << "Что там находится: " << *apartment << endl;  // Проверяем, что запись успешная...
-    
-    delete apartment; // Освободили квартиру  
-    apartment = nullptr; // Выбросили ключи («Больше не живу тут!»)  
+    int arr[] {1, 4, 7, 8};
+    for (int i = 0; i < 4; i++) {
+        cout << arr[i] << endl;
+    }
     return 0;
 }
