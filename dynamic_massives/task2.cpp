1b847ffea249892f0628dd6ad5e622a1879fad12
Jentlebright <danila.shpak@list.ru>
2026-02-07 23:53:16 +0300

    ch

---
+++ dynamic_massives/task2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    int* b = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[n];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[n];
+    }
+    for (int i = 0; i < n; i++){
+        if 
+    }
+
+
+
+
+    return 0;
+}
