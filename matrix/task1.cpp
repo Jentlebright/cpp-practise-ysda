2e1a40d65a3f779ec9b6ed6cf09f1c89ffc66aaf
Jentlebright <danila.shpak@list.ru>
2026-02-14 19:42:55 +0300

    matrix ex1

---
+++ matrix/task1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int rows, cols;
+    cin >> rows;
+    cin >> cols;
+    int **matrix;
+    matrix = new int *[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
