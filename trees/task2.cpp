0210d5b339af4370673582195677d6cf9ab08c81
Jentlebright <danila.shpak@list.ru>
2026-02-22 19:34:31 +0300

    s

---
+++ trees/task2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int **matrix;
+    matrix = new int *[n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << endl;
+        int num = 1;
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == 1) {
+                cout << num << " ";
+                num++;
+            }
+            else {
+                num++;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
